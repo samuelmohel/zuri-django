2f70a43697ec9b07cadad2d03eb7a99c508e021f
samuelmohel <samuelmohel@gmail.com>
2022-06-25 13:03:33 +0100

    My updated commit

--- I4G007858U9K/blog/models.py
+++ I4G007858U9K/blog/models.py
@@ -1,13 +1,44 @@
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.utils import timezone
+from django.urls import reverse
+from django.template.defaultfilters import slugify
+# Create your models here.
 
 
 class Post(models.Model):
-    Title = models.CharField(max_length=200)
-    Text = models.TextField()
-    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    Created_date = models.DateTimeField()
-    Published_date = models.DateTimeField()
+
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published")
+    )
+
+    # DB Fields
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=300, unique=True, editable=False)
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="blog_posts", null=True
+    )
+    body = models.TextField()
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="draft"
+    )
+
+    class Meta:
+        ordering = ("-publish",)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        pass 
+
+    def __str__(self):
+        return self.title
     
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"slug": self.slug})
